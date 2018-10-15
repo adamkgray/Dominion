8346cc6c994700447a3e4379f2946b387941b421
agray <agray@pch.com>
2018-10-14 23:12:34 -0400

    simulate game

--- dominion/gameplay.c
+++ dominion/gameplay.c
@@ -2,12 +2,25 @@
 
 struct table * gameplay(struct table * p_table) {
     static int turn = 1;
+    static int nc = 7;
+
+    if (nc == 16) {
+        nc = 7;
+    } else {
+        ++nc;
+    }
+
+    gain(p_table->players[p_table->turn]->discard, p_table->supply_piles[nc]);
+
+    for (int i = 0; i < 5; ++i) {
+        printf("%s\t", p_table->players[p_table->turn]->hand->cards[i]->name);
+    }
+    printf("\n");
+
     /* Cleanup phase */
     cleanup_phase(p_table);
 
     /* Pause */
-    printf("Turn: %d\n", turn++);
-    getchar();
 
     /* Next turn or game over */
     return (end_conditions(p_table)) ? NULL : gameplay(p_table);
